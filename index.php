7a0577e439fbd6530748dd146d0a2f15928fcd83
yangyi <yangyi@bilibili.com>
2016-09-06 18:34:37 +0800

    增加核心目录

--- index.php
+++ index.php
@@ -3,3 +3,24 @@
 //bestPHP框架
 
 echo 'hello word';
+
+
+function scanD($dir){
+    $list = scandir($dir);
+    foreach ($list as $value) {
+        if (is_dir($value)) {
+            touch($dir .'/'. $value. '/'. '.gitkeep');
+            $list = scandir($dir .'/'. $value);
+            foreach ($list as $value2) {
+                if (is_dir($value)) {
+                    touch($dir .'/'. $value. '/'. $value2 .'/' . '.gitkeep');
+                }
+            }
+
+        } else {
+
+        }
+    }
+}
+
+scanD('./');
